9566f3cf16789d7df3c017a0942612e9eb5c3dd1
Elsie4ever <3467996@gmail.com>
2022-11-19 08:44:14 +0000

    fix dev container wire up issue

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,3 @@
+npm install
+sudo chown root /workspaces/azure-iot-explorer/node_modules/electron/dist/chrome-sandbox
+sudo chmod 4755 /workspaces/azure-iot-explorer/node_modules/electron/dist/chrome-sandbox
